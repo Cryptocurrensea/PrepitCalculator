f7e4bf0c5b33dca095a5caf783215ffe82c3f67d
Jonathan Gan <jonathang132298@gmail.com>
2019-08-07 23:48:45 -0400

    First commit

---
+++ src/build/precache-manifest.c7078806802c4a2ebfca310a7847c7dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a1af0d324d35532b87547d219c62ae6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe61bbda52a87f4735a5",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "e63fc3b11bd576cc790a",
+    "url": "/static/css/main.0221d3c8.chunk.css"
+  },
+  {
+    "revision": "fe61bbda52a87f4735a5",
+    "url": "/static/js/2.cdd1958d.chunk.js"
+  },
+  {
+    "revision": "e63fc3b11bd576cc790a",
+    "url": "/static/js/main.5f3e1cf7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8d29e701138b60e6532742b38137c0cd",
+    "url": "/static/media/logo.8d29e701.svg"
+  }
+]);
---
+++ src/server.js
@@ -0,0 +1,72 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { MongoClient } from 'mongodb';
+import path from 'path';
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/build')))
+app.use(bodyParser.json());
+
+const withDB = async(opertions, res) => {
+  try {
+
+    const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+    const db = client.db('prepitcalc');
+
+    await opertions(db);
+    client.close();
+  }
+  catch (error) {
+    res.status(500).json({ message: 'Error connecting to db', error });
+  }
+}
+
+app.get('/api/:name', async (req, res) => {
+  withDB( async (db) => {
+    const pageName = req.params.name;
+
+    const pageInfo = await db.collection('pageInfo').findOne({ name: pageName });
+    res.status(200).json(pageInfo);
+  }, res)
+})
+
+
+app.get('/api/:name/visits',async (req,res) => {
+  withDB(async(db) =>{
+    const pageName = req.params.name;
+    const pageInfo = await db.collection('pageInfo').findOne({ name: pageName });
+    await db.collection('pageInfo').updateOne({ name: pageName }, {
+      '$set': {
+        visits: pageInfo.visits + 1,
+      },
+    });
+    const updatedPageInfo = await db.collection('pageInfo').findOne({ name: pageName });
+    res.status(200).json(updatedPageInfo);
+  }, res);
+});
+
+
+
+app.post('/api/:name/add-comment', (req,res) => {
+  const { username , text } = req.body;
+  const pageName = req.params.name;
+
+  withDB(async(db) =>{
+    const pageInfo = await db.collection('pageInfo').findOne({ name: pageName });
+    await db.collection('pageInfo').updateOne({ name: pageName }, {
+      '$set': {
+        comments: pageInfo.comments.concat({ username, text}),
+      },
+    });
+    const updatedPageInfo = await db.collection('pageInfo').findOne({ name: pageName });
+    res.status(200).json(updatedPageInfo);
+
+  },res);
+});
+
+app.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname + '/build/index.html'));
+});
+
+app.listen(8000,() => console.log('Listening on port 8000')); 
